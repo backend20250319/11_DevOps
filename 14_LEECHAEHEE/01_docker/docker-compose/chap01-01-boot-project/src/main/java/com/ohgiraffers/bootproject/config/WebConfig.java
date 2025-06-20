f8ed54dd3e8a0354eda2cefb9acb7924f628f0c4
heechae <chaehee15@naver.com>
2025-06-20 11:17:47 +0900

    feat: add Dockerfile

--- 14_LEECHAEHEE/01_docker/docker-compose/chap01-01-boot-project/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
+++ 14_LEECHAEHEE/01_docker/docker-compose/chap01-01-boot-project/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
@@ -1,4 +1,18 @@
 package com.ohgiraffers.bootproject.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+//                next.js에 설정된 3000번 포트에서 오는 건 cors 설정 해제
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("") // Ingress 설정으로 내부적으로 통신이 수행되므로 CORS 설정 없이도 통신 가능
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
+
+    }
 }
