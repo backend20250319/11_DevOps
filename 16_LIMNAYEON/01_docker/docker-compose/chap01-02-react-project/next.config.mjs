9533ce78c30700a0dc051239765852ed44fc81d7
nyaeon <nyaooe@gmail.com>
2025-06-26 12:33:21 +0900

    2025-06-26 실습

--- 16_LIMNAYEON/01_docker/docker-compose/chap01-02-react-project/next.config.mjs
+++ 16_LIMNAYEON/01_docker/docker-compose/chap01-02-react-project/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         // destination: "http://localhost:8080/:path*",
         // destination: "http://host.docker.internal:8080/:path*",
         // 도커 내부에서는 localhost가 React컨테이너 자신을 의미
-        destination: "http://boot001kube:8080/:path*",
+        destination: "http://boot002ser:8001/:path*",
       },
     ];
   },
